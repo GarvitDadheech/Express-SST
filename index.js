995e434ac9aafddf6a8cc0a1b1ad36fd912f3ada
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-03 10:19:21 +0530

    added requests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -9,6 +11,31 @@ let courses = [
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.json(course);
+});
+
+app.post('/courses', (req, res) => {
+   
+    const courseName = req.body.name;
+    const course = {
+        id: courses.length + 1,
+        name: courseName
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+
+    course.name = req.body.name;
+    res.json(course);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
