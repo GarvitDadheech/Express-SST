c7783ddba6a1ed491b911d7808aac86c1220caa8
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-03 10:35:26 +0530

    added delete course with id

--- index.js
+++ index.js
@@ -38,4 +38,14 @@ app.put('/courses/:id', (req, res) => {
     res.json(course);
 });
 
+app.delete('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.json(course);
+});
+
 app.listen(3000, () => console.log('Listening on port 3000...'));
